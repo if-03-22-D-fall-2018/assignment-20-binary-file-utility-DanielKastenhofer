448d3a8abdf4bc76569cbb6ccaf2e0c959100f97
DanielKastenhofer <daniel.kastenhofer1@gmail.com>
2019-06-05 21:39:30 +0200

    All working out of update_student

--- init.c
+++ init.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+    FILE* fd=fopen("student.dat","w");
+    int id=1;
 
-  FILE* fd=fopen("students.dat","w+");
-  int id = 1;
-
-  fwrite(&id, sizeof(int), sizeof(int), fd);
-  fclose(fd);
-
-  return 0;
+    fwrite(&id,sizeof(int),1,fd);
+    fclose(fd);
+    return 0;
 }
--- new_student.c
+++ new_student.c
@@ -1,37 +1,38 @@
-#include "stdio.h"
+#include <stdio.h>
 
-#define length 64
+#define Length 64
 
-int main(int argc, char const *argv[]) {
-  char first_name[length];
-  char last_name[length];
-  int age;
+int main(int argc, char const *argv[])
+{
+    char first_name[Length];
+    char last_name[Length];
+    int age;
 
-  printf("First Name: ");
-  scanf("%s", first_name);
+    printf("First Name: ");
+    scanf("%s", first_name);
 
-  printf("Last Name: ");
-  scanf("%s", last_name);
+    printf("Last Name: ");
+    scanf("%s", last_name);
 
-  printf("Age: ");
-  scanf("%d", &age);
+    printf("Age: ");
+    scanf("%d", &age);
 
-  FILE* fd = fopen("students.dat", "rw");
-  int id = 0;
+    FILE* fd=fopen("student.dat","r+");
+    int id;
 
-  fread(&id, sizeof(int), 1, fd);
+    fseek(fd,0,SEEK_SET);
+    fread(&id,sizeof(int),1,fd);
+    fseek(fd,0,SEEK_END);
 
+    fwrite(&id,sizeof(id),1,fd);
+    fwrite(first_name,sizeof(first_name),1,fd);
+    fwrite(last_name,sizeof(last_name),1,fd);
+    fwrite(&age,sizeof(age),1,fd);
 
-  fseek(fd, 0, SEEK_END);
-  fwrite(&id, sizeof(int), sizeof(int), fd);
-  fwrite(first_name, sizeof(char), sizeof(first_name), fd);
-  fwrite(last_name, sizeof(char), sizeof(last_name), fd);
-  fwrite(&age, sizeof(int), sizeof(int), fd);
-  fseek(fd, 0, SEEK_SET);
-  id++;
-  fwrite(&id, sizeof(int), sizeof(int), fd);
+    fseek(fd,0,SEEK_SET);
 
-  fclose(fd);
-
-  return 0;
+    id++;
+    fwrite(&id,sizeof(int),1,fd);
+    fclose(fd);
+    return 0;
 }
